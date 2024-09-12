646212027c39f7ccfdec7252709af73642798c4d
anthonyf <anthonyf@genstudents.org>
2024-09-12 11:33:47 +0100

    shared repo

---
+++ python-modules-NAT3-main/README.md
@@ -0,0 +1,2 @@
+# python-modules-NAT3
+Location for Python modules from the Generation AWS course. 
---
+++ test.py
@@ -0,0 +1,40 @@
+# test push
+
+#1. Store a message in a variable, then print that message. 
+my_name = "anthony"
+print(my_name)
+
+#2. Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+my_name = "anthony france"
+print(my_name)
+
+#3. Store a person’s name in a variable, and print a message to that person. 
+
+
+#4. Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+print(my_name.lower(), my_name.upper(), my_name.title())
+
+#5. Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+# Be sure to enclose your operations in print statements to see the results.
+print(1+7, 9-1, 2*4,16/2)
+
+#6. Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+my_fav_number = 3
+print(f"my favourite number is {my_fav_number}".title())
+
+
+## Stretch and Challenge 
+#1. Use an f-string to print out a message with a variable. 
+
+#2. Use multiple variables in one string to print a message.
+my_firstname = "Anthony"
+my_surname = 'France'
+print(f"My name is {my_firstname} {my_surname}")
+
+#3. Set three variables, one that says, “Good”, another that says, “Day”, and another that says your name. “Add” them together, with a space between each word, and store it into another variable. Then, print out that variable. 
+greet_good = "Good"
+greet_day = "Day"
+print(f"{greet_good} {greet_day} {my_firstname}")
+
+###########################################
+
